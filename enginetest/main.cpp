914bafa8a71f54a25dad4f1a891c3acbdc684cda
explorer85 <explorer.85@mail.ru>
2018-12-13 23:05:02 +0300

    тестовый пример иерархии объектов с использованием ромбовидного наследования

---
+++ enginetest/main.cpp
@@ -0,0 +1,106 @@
+#include <QCoreApplication>
+
+#include <QDebug>
+
+class Base {
+public:
+	virtual ~Base() = default;
+	void test() {}
+};
+
+class IRenderable : virtual public Base {
+public:
+	virtual void render() = 0;
+};
+
+class Inputable : virtual public Base {
+	public:
+	virtual void input(int mouse) = 0;
+};
+
+class MapObject : public IRenderable, public Inputable {
+public:
+	void render() {  qDebug() << "MapObject::render"; }
+	void input(int /*mouse*/) { qDebug() << "MapObject::input";  }
+};
+
+class Gridbject : public IRenderable {
+public:
+	void render() { qDebug() << "Gridbject::render";  }
+};
+
+
+class Engine {
+public:
+//	template <class T>
+//	void addObject(T obj) {
+
+//		objects.push_back(obj);
+//		//obj->input(5);
+//		if (static_cast<IRenderable*>(obj)) {
+//			irobjects.push_back(obj);
+//		}
+//		if (static_cast<Inputable*>(obj)) {
+//			//Inputable* i = obj;
+//			iiobjects.push_back(obj);
+//		}
+//	}
+
+
+
+	void addObject(Base* obj) {
+
+		objects.push_back(obj);
+		//obj->input(5);
+		if (IRenderable* ir = dynamic_cast<IRenderable*>(obj)) {
+			irobjects.push_back(ir);
+		}
+		if (Inputable* ii = dynamic_cast<Inputable*>(obj)) {
+			iiobjects.push_back(ii);
+		}
+	}
+
+	void update() {
+			for (IRenderable* ptr : irobjects) {
+				ptr->render();
+			}
+
+			for (Inputable* ptr : iiobjects) {
+				ptr->input(5);
+			}
+	}
+
+	std::vector<IRenderable*> irobjects;
+	std::vector<Inputable*> iiobjects;
+	std::vector<Base*> objects;
+
+};
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+
+
+
+	MapObject *map = new MapObject();
+	Gridbject *grid = new Gridbject();
+
+   Inputable* res = (Inputable*)(grid);
+
+	Engine e;
+//	e.addObject<MapObject*>(map);
+//	e.addObject<Gridbject*>(grid);
+
+	e.addObject(map);
+	e.addObject(grid);
+
+
+	e.update();
+
+
+//	for (Base* ptr : objects) {
+//		ptr
+//	}
+
+	return a.exec();
+}
