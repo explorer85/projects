f64850314f67c93926b076e195cbd4fab6a26e8d
Valery Rodin <explorer.85@mail.ru>
2021-03-12 11:22:03 +0300

    добавил тест

--- jsonvisualizer/tests/jsonparser/jsonparsertest.cpp
+++ jsonvisualizer/tests/jsonparser/jsonparsertest.cpp
@@ -6,13 +6,28 @@ class JsonParserTest : public QObject {
 private slots:
     void testReadJson();
 
+
+
 };
 
 void JsonParserTest::testReadJson() {
     JsonParser jp;
-    jp.formatFromString("{}");
+    //empty
+    QString excpectedString = "{\n}\n";
+    QString outString = jp.formatFromFile("empty.json");
+    QCOMPARE(outString, excpectedString);
 
-    //QCOMPARE()
+    //keyvalue
+    excpectedString = "{\n       \"key1\": \"value1\",\n       \"key2\": \"value2\"\n}\n";
+    outString = jp.formatFromFile("keyvalue.json");
+    QCOMPARE(outString, excpectedString);
+
+    //array of key value
+    excpectedString = "{\n       \"key1\": [\n              {\n                     \"obj1key1\": \"obj1value1\",\n"
+                      "                     \"obj1key2\": \"obj1value2\"\n              },\n              {\n"
+                      "                     \"obj2key1\": \"obj2value1\",\n                     \"obj2key2\": \"obj2value2\"\n              }\n       ]\n}\n";
+    outString = jp.formatFromFile("arrayofvalue.json");
+    QCOMPARE(outString, excpectedString);
 
 }
 
