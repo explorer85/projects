d856e6b5db7bca6a4f88139e887b193a5d92035e
Valery Rodin <explorer.85@mail.ru>
2022-12-26 08:53:18 +0300

    cs

--- RpcService/main.cpp
+++ RpcService/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
     serv.onReceiveMessage(data);
   }
 
-  // AddTargetMessage
+  // RemoveTargetMessage
   {
     RemoveTargetMessage msg;
     msg.number = 10;
